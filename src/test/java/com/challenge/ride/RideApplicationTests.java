3c0ea018957bc76dda74c1d5b7bb79473843f8a4
Eric Arden <ardeneric77@gmail.com>
2024-09-09 15:32:45 +0000

    Project Initialized - dependencies - Springboot - mvaen

---
+++ src/test/java/com/challenge/ride/RideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.ride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
